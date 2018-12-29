6f3a7a1bf816d49182cb52867bc2ef3eecd2a305
Snewkis <46242196+Snewkis@users.noreply.github.com>
2018-12-29 19:17:20 +0100

    Add a demo for the if statement

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -17,5 +17,13 @@ let eachArray = each(users, (item, i) => { return `
 	<li>User ${i} : name - ${item.name}, age - ${item.age}</li>
 `});
 
+let testIf = 3;
+
+let testTrue = ifState(testIf == 3, `TRUE ${testIf} = 3`, `FALSE ${testIf} ≠ 3`);
+let testFalse = ifState(testIf == 2, `TRUE ${testIf} = 2`, `FALSE ${testIf} ≠ 2`);
+
 insert(forLoopList, "#forLoopId");
 insert(eachArray, "#eachId");
+
+insert(testTrue, "#trueTest");
+insert(testFalse, "#falseTest");
