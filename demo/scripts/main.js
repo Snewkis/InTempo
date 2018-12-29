faef1d33f653b8db61cc5036640bac63f3257d92
BuildTools <unconfigured@null.spigotmc.org>
2018-12-29 18:25:34 +0100

    Add a demo for the each function.

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -2,4 +2,20 @@ let forLoopList = forLoop(2, (item, i) => { return `
 	<li>Item ${i}</li>
 `})
 
+let users = [
+	{
+		name: "John Doe",
+		age: 21
+	},
+	{
+		name: "John Die",
+		age: 22
+	}
+]
+
+let eachArray = each(users, (item, i) => { return `
+	<li>User ${i} : name - ${item.name}, age - ${item.age}</li>
+`});
+
 insert(forLoopList, "#forLoopId");
+insert(forLoopList, "#eachId");
